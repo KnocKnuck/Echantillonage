e4f5db70a7d5d0f4c192fe4297c6f5065e819626
Joseph HANI <53782645+KnocKnuck@users.noreply.github.com>
2020-09-29 10:38:00 +0200

    Add files via upload

---
+++ TD1.R
@@ -0,0 +1,43 @@
+### TD1 STATISTIQUES ET APPLICATIONS 
+setwd("D:/One Drive/OneDrive/Univ Joseph/Universit� Lille 3 - MIASHS/Master 1 SID/Semestre 2/Visualisation de l'information - FRAISSE/LAVIGNE/TD 1-20170928")
+table=read.table('PopAireFixe.csv', sep=';',dec='.', header = TRUE)
+head(table)
+ech = c(20,66,98,104,81,24,48,13,38,16,11,30)
+a_ech = table$aire[ech]
+mean(a_ech)
+sd(a_ech)
+moy = c(54.82,41.83,54.10,56.45,46.01,47.59,47.60,58.81,57.54,54.83,53.04,47.75,59.32)
+mean(moy)
+mu = mean(table$aire)
+hist(moy)
+ech2 = sample(1:114,12)
+mean(ech2)
+
+
+### PARTIE 2 EXERCICE 2 
+
+head(table)
+hist(moy)
+
+table$type
+
+##Echantillonage Stratifié 
+## 3 Cercles 
+
+ech_cercles = sample(1:30,3)
+moy_cercles = mean(table$aire[ech_cercles])
+
+## 4 Triangles 
+
+ech_triangle = sample(31:67,4)
+moy_triangle = mean(table$aire[ech_triangle])
+
+## 5 Carré 
+
+ech_carrés = sample(68:114,5)
+moy_carrés = mean(table$aire[ech_carrés])
+
+## EStimation de la moyenne 
+
+moy_str = 0.26*moy_cercles+0.33*moy_triangle+0.41*moy_carrés
+
